32372d5c48ca36297853992876a7d3fdb9762e75
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-02-24 17:02:16 +0500

    update

--- Assignment  # 05/task1.js
+++
@@ -1,8 +0,0 @@
-var number = Number(prompt('Enter a positive integer'))
-var fact = 1;
-
-for (i = 1; i <= number; i++) {
-    fact *= i;
-}
-
-document.write(`The factorial of ${number} is ${fact}`);
